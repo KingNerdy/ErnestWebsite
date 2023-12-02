3d7a3357109df81c1f0ef2fa6c83d5db5e611ef6
KingNerdy <ernest.tanhk@gmail.com>
2023-12-02 09:27:20 +0800

    fix: edited index.html

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,7 @@ import postcssNesting from 'postcss-nesting';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'build'
-  },
+  base: '/ErnestWebsite/',
   css: {
     postcss: {
         plugins: [
